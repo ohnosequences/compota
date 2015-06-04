75b283051f9f71210ca9bcb1fa829dab70355acb
evdokim <ekovach@ohnosequences.com>
2015-06-04 19:12:12 +0200

    unification and simplication 2

---
+++ src/main/scala/ohnosequences/compota/aws/AwsNispero.scala
@@ -0,0 +1,55 @@
+package ohnosequences.compota.aws
+
+import ohnosequences.compota._
+import ohnosequences.compota.aws.queues.DynamoDBContext
+import ohnosequences.compota.local.{LocalNisperoConfiguration, LocalEnvironment}
+import ohnosequences.compota.queues._
+
+
+trait AnyAwsNispero extends AnyNispero {
+  override type NisperoConfiguration = AwsNisperoConfiguration
+  override type NisperoEnvironment = AwsEnvironment
+}
+
+
+
+
+class AwsNispero[In, Out, InCtx, OutCtx, InQueue <:  AnyQueue.of2[In, InCtx], OutQueue <: AnyQueue.of2[Out, OutCtx]](
+                                                                      val inputQueue: InQueue,
+                                                                      val inputContext: AwsEnvironment => InCtx,
+                                                                      val outputQueue: OutQueue,
+                                                                      val outputContext: AwsEnvironment => OutCtx,
+                                                                      val instructions: Instructions[In, Out],
+                                                                      val configuration: AwsNisperoConfiguration)
+  extends AnyAwsNispero {
+
+  override type NisperoConfiguration = AwsNisperoConfiguration
+
+  override type NisperoInput = In
+  override type NisperoOutput = Out
+
+  override type NisperoInputContext = InCtx
+  override type NisperoOutputContext = OutCtx
+
+  override type NisperoInputQueue = InQueue
+  override type NisperoOutputQueue = OutQueue
+}
+
+
+
+
+
+object AwsNispero {
+  def apply[In, Out, InQueue <:  AnyQueue.of2[In, DynamoDBContext], OutQueue <:  AnyQueue.of2[Out, DynamoDBContext]](
+                                                                                inputQueue: InQueue,
+                                                                                outputQueue: OutQueue,
+                                                                                instructions: Instructions[In, Out],
+                                                                                configuration: AwsNisperoConfiguration):
+  AwsNispero[In, Out, DynamoDBContext, DynamoDBContext, InQueue, OutQueue] = new AwsNispero[In, Out, DynamoDBContext, DynamoDBContext, InQueue, OutQueue](
+  inputQueue, {t: AwsEnvironment => t.createDynamoDBContext()},
+  outputQueue,{t: AwsEnvironment => t.createDynamoDBContext()},
+  instructions,
+  configuration)
+}
+
+
